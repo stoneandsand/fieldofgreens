324d26b6c14289cdfcc7d585148fc53483f4c81e
Grace Ko <grace.eunhyae.ko@gmail.com>
2017-11-16 23:37:19 -0500

    frontend merge

--- client/src/components/itemEntry.jsx
+++ client/src/components/itemEntry.jsx
@@ -3,13 +3,12 @@ import Recalls from './recalls.jsx';
 
 let itemEntry = ({entry, deleteItem, i}) => (
 
-
-
-
 <div>
-    <div>{entry.name}</div>
-    <button onClick={deleteItem.bind(this, i)} >delete</button>
-    <div></div>
+  <div>{entry.name}</div>
+  <button onClick={deleteItem.bind(this, i)} >delete</button>
+  <div>
+    <Recalls recall={entry.recalls ? entry.recalls[0].recallDescription : ""}/>
+  </div>
 </div>
   )
 
