913d045b02bfc4699fd551c9bff8f1213a364519
dwrz <dwrz@dwrz.net>
2017-11-30 23:01:02 +0000

    Refactor styling for saveList component

--- client/src/components/savelist.jsx
+++ client/src/components/savelist.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const saveList = props => (
   <div>
-    <form className="save-list" onSubmit={props.saveGroceryListName}>
-      <label>
-        <input type="text" className="item-input" placeholder="List Name" value={props.inputListName} onChange={props.updateGroceryListName} />
-      </label>
-      <input type="submit" className="btn btn-success" value="Save List" />
+    <form className="form-group" onSubmit={props.saveGroceryListName}>
+      <div className="input-group">
+        <input type="text" className="form-control" placeholder="List Name" value={props.inputListName} onChange={props.updateGroceryListName} />
+        <span className="input-group-btn">
+        <input type="submit" className="btn btn-success" value="Save List" />
+      </span>
+      </div>
     </form>
   </div>
 );
