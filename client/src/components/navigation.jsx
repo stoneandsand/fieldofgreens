3218281b0d5ba4ddef4bb2ec95ce54eabbac365f
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-30 11:03:26 -0800

    Conditionally render login/logout based on state

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -13,6 +13,9 @@ class Navigation extends React.Component {
       <div className="container-fluid navigation">
         <div className="row">
           <div className="col-md-4 loginOrOut">
+            {
+              this.state.isLoggedIn ? <h1>Logout</h1> : <h1>Login</h1>
+            }
           </div>
           <div className="col-md-4 search">
           </div>
