4bf25d6cdac212565205747f7b9fc379753ed7ca
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-29 19:38:20 -0800

    Build navigation component

---
+++ client/src/components/navigation.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>test</h1>
+    );
+  }
+}
