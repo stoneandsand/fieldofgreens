069b3ec360b685969cf4ed13573484cfded705cc
dwrz <dwrz@dwrz.net>
2017-11-30 22:43:30 +0000

    Refactor variables from grossery to grocery

--- client/src/components/savelist.jsx
+++ client/src/components/savelist.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const saveList = props => (
   <div>
-    <form className="save-list" onSubmit={props.saveGrosseryListName}>
+    <form className="save-list" onSubmit={props.saveGroceryListName}>
       <label>
-        <input type="text" className="item-input" placeholder="New List Name" value={props.inputListName} onChange={props.updateGrosseryListName} />
+        <input type="text" className="item-input" placeholder="List Name" value={props.inputListName} onChange={props.updateGroceryListName} />
       </label>
       <input type="submit" className="btn btn-success" value="Save List" />
     </form>
