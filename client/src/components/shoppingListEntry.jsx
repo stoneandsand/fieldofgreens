20e069bcace4a28482bb77586ed0786469f28c34
dwrz <dwrz@dwrz.net>
2017-11-30 18:54:24 +0000

    Delete shoppingListEntry component

--- client/src/components/shoppingListEntry.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const shoppingListEntry = props => (
-  <div onClick={props.onClick.bind(this, props.listEntry)}>
-    {props.listEntry}
-  </div>
-);
-
-export default shoppingListEntry;
