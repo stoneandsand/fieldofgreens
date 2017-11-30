551d1c2adc9e0d3f9a3161772979851d5c6a0667
Alexander Mire <amire51117@gmail.com>
2017-11-30 15:22:43 -0500

    added auth0 to index.html and made auth config

---
+++ Auth/Auth0-settings.js
@@ -0,0 +1,20 @@
+export const AUTH_SETTINGS = {
+  domain: 'field-of-greens.auth0.com',
+  clientId:'',
+  callbackUrl: 'http://localhost:5000:callback',
+  options{
+    languageDictionary: {
+      title: "fieldOfGreens"
+    },
+    theme: {
+      primaryColor: '#71d839',
+      socialButtonStyle:'small'
+    },
+    allowedConnections: ['facebook', 'linkedin', 'Username-Password-Authentication'],
+    allowLogin: true,
+    loginAfterSignUp: true,
+    closable: true,
+    autoclose: true,
+    oidcConformant: false,
+  }
+}
