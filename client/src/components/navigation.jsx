df6ce07e4dce17642d849b8775c9a1d5237b144a
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-30 11:01:12 -0800

    Add state for logged in statuse, create 3 columns

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -3,12 +3,22 @@ import React from 'react';
 class Navigation extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isLoggedIn: false,
+    }
   }
 
   render() {
     return (
       <div className="container-fluid navigation">
-        <h1>test</h1>
+        <div className="row">
+          <div className="col-md-4 loginOrOut">
+          </div>
+          <div className="col-md-4 search">
+          </div>
+          <div className="col-md-4 signUpOrSettings">
+          </div>
+        </div>
       </div>
     );
   }
