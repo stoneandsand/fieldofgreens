d5c4feeba8dd7896b0b8dfb0eaae7a05094e8bbe
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-01 11:08:37 -0800

    Change destructured props to just props

--- client/src/components/iteminput.jsx
+++ client/src/components/iteminput.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const itemInput = ({newItemEntry, updateNewItemEntry, addNewItemToList, }) => (
+const itemInput = props => (
   <div>
-    <form className="form-group" onSubmit={addNewItemToList}>
+    <form className="form-group" onSubmit={props.addNewItemToList}>
       <div className="input-group">
-      <input className="form-control" type="text" placeholder="New Item" value={newItemEntry} onChange={updateNewItemEntry} />
+      <input className="form-control" type="text" placeholder="New Item" value={props.newItemEntry} onChange={props.updateNewItemEntry} />
       <span className="input-group-btn">
         <input type="submit" className="btn btn-success" value="Add Item" />
       </span>
