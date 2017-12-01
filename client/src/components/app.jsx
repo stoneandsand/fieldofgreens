0240e762ff22537574a56b7878881de2863fd80e
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-30 19:48:31 -0800

    Add isLoggedIn state to app state

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -15,6 +15,7 @@ class App extends React.Component {
       currentItems: [],
       newItemEntry: '',
       inputListName: '',
+      isLoggedIn: false,
       savedListName: '',
       savedListsfromDB: [],
     };
