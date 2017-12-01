e1e273d22b4dc5416152275461912a64ecd3e5f2
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-01 11:51:42 -0800

    Include updateNewItemEntry in passed props to ItemInput

--- client/src/components/navigation.jsx
+++ client/src/components/navigation.jsx
@@ -26,7 +26,7 @@ class Navigation extends React.Component {
             <h1>The Grossery List</h1>
           </div>
           <div className="col-md-4 search">
-            <ItemInput newItemEntry={this.props.newItemEntry} addNewItemToList={this.props.addNewItemToList} />
+            <ItemInput addNewItemToList={this.props.addNewItemToList} newItemEntry={this.props.newItemEntry} updatedNewItemEntry={this.props.updateNewItemEntries} />
           </div>
           <div className="col-md-4 signUpOrSettings">
             <StateDropdown location={this.props.location} selectState={this.props.selectState} />
