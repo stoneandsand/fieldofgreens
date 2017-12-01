890d561130a2b03d61137020755d45db324ccc00
Charles Paul Bennett <cpbennett4@gmail.com>
2017-11-30 19:44:59 -0800

    Set state.state to empty string

--- client/src/components/app.jsx
+++ client/src/components/app.jsx
@@ -11,7 +11,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      state: 'CA',
+      state: '',
       currentItems: [],
       newItemEntry: '',
       inputListName: '',
