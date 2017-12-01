11e57288225b02257610f92ed3abcc9581700293
dwrz <dwrz@dwrz.net>
2017-12-01 03:45:55 +0000

    Fix key in currentItems

--- client/src/components/currentItems.jsx
+++ client/src/components/currentItems.jsx
@@ -2,15 +2,15 @@
 
 import React from 'react';
 
-const currentItems = props => {
-  return <h1>test</h1>
-  /*return (
-  <div>
-    <p><b>Current Items:</b></p>
-    {props.currentItems.map((item, i) => (
-      <span key={i}>{item} </span>
-    ))}
-  </div>
-)*/};
+const currentItems = props =>
+      (
+        <div>
+          <p><b>Current Items:</b></p>
+          {props.currentItems.map((item, i) => {
+            (
+              <span key={`currentItems-${i}`}>{item} </span>
+            )})}
+        </div>
+      );
 
 export default currentItems;
