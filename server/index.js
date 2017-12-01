60dd56831a2aa4c8d6b201dad955c0e1b8f618a0
Jonathan Tang <j.tang17@gmail.com>
2017-12-01 13:51:13 -0500

    Run ESlint on server/index.js and refactor for errors

--- server/index.js
+++ server/index.js
@@ -1,37 +1,34 @@
-const express = require ('express');
-let app = express();
-let bodyParser = require('body-parser');
-let recalls = require('../db/data.js');
-let mongoose = require('mongoose');
-let db = require('../db/index.js');
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const recalls = require('../db/data.js');
+const db = require('../db/index.js');
 
 app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/../'));
+app.use(express.static(`${__dirname}/../`));
 app.use(bodyParser.json());
 
 
 // Helper function for getting data that matches the recalls GET request`
 
-let getRecallMatches = (keywordsArray) => {
+const getRecallMatches = (keywordsArray) => {
   let matches = [];
-  for (let i = 0; i < keywordsArray.length; i++) {
-    for (let k = 0; k < recalls.recallData.length; k++) {
-      if (recalls.recallData[k]['product_description'].toUpperCase().includes(keywordsArray[i].toUpperCase()) && recalls.recallData[k]['report_date'] > '20160427') {
+  for (let i = 0; i < keywordsArray.length; i + 1) {
+    for (let k = 0; k < recalls.recallData.length; k + 1) {
+      if (recalls.recallData[k].product_description.toUpperCase().includes(keywordsArray[i].toUpperCase()) && recalls.recallData[k].report_date > '20160427') {
         matches.push(recalls.recallData[k]);
       }
     }
   }
-  matches = matches.filter(function(match) {
-    return match.status === 'Ongoing'
-
-  })
-  return matches
-}
- // || recalls.recallData[k]['brand name'].toUpperCase().includes(keywordsArray[i].toUpperCase()
+  matches = matches.filter(match => match.status === 'Ongoing');
+  return matches;
+};
+// || recalls.recallData[k]['brand name'].toUpperCase().includes(keywordsArray[i].toUpperCase()
 
 app.post('/signup', (req, res) => {
   console.log(req.body);
-  //expecting {username: '', password: ''}
+  // expecting {username: '', password: ''}
   db.signup(req.body, (username) => {
     if (username) {
       res.send(username);
@@ -41,19 +38,17 @@ app.post('/signup', (req, res) => {
   });
 });
 
-//GET request for getting recall data from data.js
+// GET request for getting recall data from data.js
 app.get('/api/search', (req, res) => {
   // expecting {item: 'cheese cake', location: 'CA'}
-  console.log('REQUEST FROM CLIENT', req.body)
+  console.log('REQUEST FROM CLIENT', req.body);
 
-  let keywords = JSON.parse(req.body.item).name.split(' ');
+  const keywords = JSON.parse(req.body.item).name.split(' ');
   console.log(keywords);
 
   let matches = getRecallMatches(keywords);
 
-  matches = matches.filter(function(match) {
-    return match['distribution_pattern'].includes(req.body.location) || match['distribution_pattern'].includes('Nationwide');
-  })
+  matches = matches.filter(match => match.distribution_pattern.includes(req.body.location) || match.distribution_pattern.includes('Nationwide'));
   matches.unshift(JSON.parse(req.body.item).name);
   console.log('matches ======>', matches);
   res.send(matches.slice(0, 11));
@@ -61,15 +56,15 @@ app.get('/api/search', (req, res) => {
 
 
 // POST request for saving new list to database
-app.post('/api/users/:username/lists', function(req, res) {
+app.post('/api/users/:username/lists', (req, res) => {
   // expecting {username: '', items: ['apples', 'oranges'], listName: 'fruits'}
-  let items = [];
-  for (let item of req.body.items) {
+  const items = [];
+  for (const item of req.body.items) {
     items.push(item);
   }
-  let list = {
+  const list = {
     name: req.body.listName,
-    items: items
+    items,
   };
   console.log(list);
   db.saveList(req.body.username, list, (updatedLists) => {
@@ -78,11 +73,11 @@ app.post('/api/users/:username/lists', function(req, res) {
   });
 });
 
-// GET request for retrieving all saved lists names for rendering Saved Shopping List component of a user
+// GET request for retrieving all saved lists names for a user
 app.get('/api/users/:username/lists', (req, res) => {
   db.getUserLists(req.params.username, (userLists) => {
     console.log(userLists);
-    res.send(userLists)
+    res.send(userLists);
   });
 });
 
@@ -96,21 +91,21 @@ app.get('/api/users/:username/lists/:id', (req, res) => {
 });
 
 app.get('/api/users/:username/allergies', (req, res) => {
-  //retrieve user's allergy list
+  // retrieve user's allergy list
   db.findAllergies(req.params.username, (allergyList) => {
     res.send(allergyList);
   });
 });
 
 app.get('/api/users/:username/likes', (req, res) => {
-  //retrieve user's like list
+  // retrieve user's like list
   db.findLikes(req.params.username, (likeList) => {
     res.send(likeList);
   });
 });
 
 app.get('/api/users/:username/dislikes', (req, res) => {
-  //retrieve user's dislike list
+  // retrieve user's dislike list
   db.findDislikes(req.params.username, (dislikeList) => {
     res.send(dislikeList);
   });
@@ -118,33 +113,33 @@ app.get('/api/users/:username/dislikes', (req, res) => {
 
 app.post('/api/users/:username/allergies', (req, res) => {
   console.log(req.body.item);
-  //Expecting {item: 'apple'} item in POST request
+  // Expecting {item: 'apple'} item in POST request
   db.addAllergies(req.params.username, req.body.item, (updatedAllergyList) => {
     res.send(updatedAllergyList);
   });
 });
 
 app.post('/api/users/:username/likes', (req, res) => {
-  //Expecting {item: 'apple'} item in POST request
+  // Expecting {item: 'apple'} item in POST request
   db.addLikes(req.params.username, req.body.item, (updatedLikeList) => {
     res.send(updatedLikeList);
   });
 });
 
 app.post('/api/users/:username/dislikes', (req, res) => {
-  //Expecting {item: 'apple'} item in POST request
+  // Expecting {item: 'apple'} item in POST request
   db.addAllergies(req.params.username, req.body.item, (updatedDislikeList) => {
     res.send(updatedDislikeList);
   });
 });
 
 app.post('/api/users/:username/delete', (req, res) => {
-  //Expecting {name: 'apple', type: 'allergies'}
+  // Expecting {name: 'apple', type: 'allergies'}
   db.removeItem(req.params.username, req.body, (updatedList) => {
-    res.send(updatedList)
+    res.send(updatedList);
   });
-})
+});
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Server running on port', app.get('port'));
 });
