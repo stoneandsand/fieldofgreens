ea9dedb760ad27321fb2985fb858d48d51ba39ae
dwrz <dwrz@dwrz.net>
2017-12-01 03:58:08 +0000

    Add comma separation to currentItems display

--- client/src/components/currentItems.jsx
+++ client/src/components/currentItems.jsx
@@ -1,16 +1,18 @@
 // Component to display items currently selected by the user.
+// TODO: Refactor span logic (clean up).
 
 import React from 'react';
 
 const currentItems = props =>
       (
         <div>
-          <p><b>Current Items:</b></p>
-          {props.currentItems.map((item, i) => {
-            (
-              <span key={`currentItems-${i}`}>{item} </span>
-            )})}
-        </div>
+          <p><b>Current Items: </b>
+            {props.currentItems.map((item, i) => {
+              return (
+                <span key={`currentItems-${i}`}>{item}{props.currentItems.length <= 1 ? '.' : i < props.currentItems.length - 1 ? ', ' : '.'}</span>
+              )})}
+        </p>
+          </div>
       );
 
 export default currentItems;
