497749b4c36642f9ab78d84bf8e2d9d378aa4a38
Jonathan Tang <j.tang17@gmail.com>
2017-11-30 19:21:12 -0500

    Add Create/Read server and db functions for like, dislike, allergy

--- db/index.js
+++ db/index.js
@@ -3,13 +3,10 @@ const DB_URI = process.env.MONGODB_URI ? `${process.env.MONGODB_URI}/fieldofgree
 
 const User = require('./schemas.js');
 
-mongoose.connect(DB_URI);
+mongoose.connect(DB_URI, {useMongoClient: true});
 
 let db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('connection success');
-});
 
 const signup = (data, callback) => {
   User.findOne({username: data.username}, (err, userEntry) => {
@@ -48,7 +45,7 @@ const getUserLists = (user, callback) => {
         callback(userEntry.lists);
       }
     }
-  })
+  });
 };
 
 // Saves new list for user to database
@@ -89,6 +86,106 @@ const findList = (user, id, callback) => {
   });
 };
 
+const findAllergies = (user, callback) => {
+  User.findOne({username: user}, (err, userEntry) => {
+    if (err) {
+      console.error(err)
+    } else {
+      if (!userEntry) {
+        console.log('no user found');
+        callback([]);
+      } else {
+        callback(userEntry.allergies);
+      }
+    }
+  });
+};
+
+const addAllergies = (user, item, callback) => {
+  User.findOne({username: user}, (err, userEntry) => {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log(item);
+      userEntry.allergies.push(item);
+      userEntry.save((err, updatedEntry) => {
+        if (err) {
+          console.error(err);
+          callback([]);
+        } else {
+          callback(updatedEntry.allergies);
+        }
+      });
+    }
+  });
+};
+
+const findLikes = (user, callback) => {
+  User.findOne({username: user}, (err, userEntry) => {
+    if (err) {
+      console.error(err)
+    } else {
+      if (!userEntry) {
+        console.log('no user found');
+        callback([]);
+      } else {
+        callback(userEntry.likes);
+      }
+    }
+  });
+};
+
+const addLikes = (user, item, callback) => {
+  User.findOne({username: user}, (err, userEntry) => {
+    if (err) {
+      console.error(err);
+    } else {
+      userEntry.likes.push(item);
+      userEntry.save((err, updatedEntry) => {
+        if (err) {
+          console.error(err);
+          callback([]);
+        } else {
+          callback(updatedEntry.likes);
+        }
+      });
+    }
+  });
+};
+
+const findDislikes = (user, callback) => {
+  User.findOne({username: user}, (err, userEntry) => {
+    if (err) {
+      console.error(err)
+    } else {
+      if (!userEntry) {
+        console.log('no user found');
+        callback([]);
+      } else {
+        callback(userEntry.dislikes);
+      }
+    }
+  });
+};
+
+const addDislikes = (user, item, callback) => {
+  User.findOne({username: user}, (err, userEntry) => {
+    if (err) {
+      console.error(err);
+    } else {
+      userEntry.dislikes.push(item);
+      userEntry.save((err, updatedEntry) => {
+        if (err) {
+          console.error(err);
+          callback([]);
+        } else {
+          callback(updatedEntry.dislikes);
+        }
+      });
+    }
+  });
+};
+
 //TODO FOR ADDING ITEM TO AN EXISTING LIST
 const addItemToList = (user, item, list, callback) => {
   User.findOne({username: user}, (err, userEntry) => {
@@ -100,7 +197,13 @@ const addItemToList = (user, item, list, callback) => {
   })
 };
 
-module.exports.getUserLists = getUserLists;
 module.exports.signup = signup;
+module.exports.getUserLists = getUserLists;
 module.exports.saveList = saveList;
 module.exports.findList = findList;
+module.exports.findAllergies = findAllergies;
+module.exports.addAllergies = addAllergies;
+module.exports.findLikes = findLikes;
+module.exports.addLikes = addLikes;
+module.exports.findDislikes = findDislikes;
+module.exports.addDislikes = addDislikes;
--- server/index.js
+++ server/index.js
@@ -44,25 +44,22 @@ app.post('/signup', (req, res) => {
 //GET request for getting recall data from test.js
 
 app.get('/api/search', (req, res) => {
-  console.log(req.body);
-  //expecting {item: , location: }
-  res.send(`server received ${req.body}`);
-  // for testing purposes since this endpoint is not working currently
+  console.log('REQUEST FROM CLIENT', req.body)
 
+  let keywords = JSON.parse(req.body.item).name.split(' ');
+  console.log(keywords);
 
-  // let keywords = JSON.parse(req.body.item).name.split(' ');
-  // console.log(keywords);
-  // let matches = getRecallMatches(keywords);
-  // // This commented out section is for filtering the matches to the users set location.
-  //   // matches.filter((match) => {
-  // //   match['distribution_pattern'].indexOf(JSON.parse(req.query.state)) >= 0 || match['distribution_pattern'].indexOf("Nationwide") >= 0
-  // // })
-  // matches = matches.filter(function(match) {
-  //   return match['distribution_pattern'].includes(req.body.state) || match['distribution_pattern'].includes('Nationwide');
+  let matches = getRecallMatches(keywords);
+  // This commented out section is for filtering the matches to the users set location.
+    // matches.filter((match) => {
+  //   match['distribution_pattern'].indexOf(JSON.parse(req.query.state)) >= 0 || match['distribution_pattern'].indexOf("Nationwide") >= 0
   // })
-  // matches.unshift(JSON.parse(req.body.item).name);
-  // console.log('matches ======>', matches);
-  // res.send(matches.slice(0, 11));
+  matches = matches.filter(function(match) {
+    return match['distribution_pattern'].includes(req.query.state) || match['distribution_pattern'].includes('Nationwide');
+  })
+  matches.unshift(JSON.parse(req.query.item).name);
+  console.log('matches ======>', matches);
+  res.send(matches.slice(0, 11));
 });
 
 
@@ -92,13 +89,52 @@ app.get('/api/users/:username/lists', (req, res) => {
   });
 });
 
-
 // GET request for retrieving a single saved list
 app.get('/api/users/:username/lists/:id', (req, res) => {
   db.findList(req.params.username, req.params.id, (targetList) => {
     console.log(targetList);
     res.send(targetList);
   });
+});
+
+app.get('/api/users/:username/allergies', (req, res) => {
+  db.findAllergies(req.params.username, (allergyList) => {
+    res.send(allergyList);
+  });
+});
+
+app.get('/api/users/:username/likes', (req, res) => {
+  db.findLikes(req.params.username, (likeList) => {
+    res.send(likeList);
+  });
+});
+
+app.get('/api/users/:username/dislikes', (req, res) => {
+  db.findDislikes(req.params.username, (dislikeList) => {
+    res.send(dislikeList);
+  });
+});
+
+app.post('/api/users/:username/allergies', (req, res) => {
+  console.log(req.body.item);
+  //Expecting {item: 'apple'} item in POST request
+  db.addAllergies(req.params.username, req.body.item, (updatedAllergyList) => {
+    res.send(updatedAllergyList);
+  })
+});
+
+app.post('/api/users/:username/likes', (req, res) => {
+  //Expecting string item in POST request
+  db.addLikes(req.params.username, req.body.item, (updatedLikeList) => {
+    res.send(updatedLikeList);
+  })
+});
+
+app.post('/api/users/:username/dislikes', (req, res) => {
+  //Expecting string item in POST request
+  db.addAllergies(req.params.username, req.body.item, (updatedDislikeList) => {
+    res.send(updatedDislikeList);
+  })
 });
 
 app.listen(app.get('port'), function() {
