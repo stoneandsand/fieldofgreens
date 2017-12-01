562b16cb8a08cb1984e31db304b38e569d5794ac
Jonathan Tang <j.tang17@gmail.com>
2017-11-30 23:07:32 -0500

    Fix typos in utilities functions

--- server/utilities.js
+++ server/utilities.js
@@ -1,12 +1,12 @@
 const axios = require('axios');
-const FDA_API_KEY = require('./config.js')
+const config = require('./config.js')
 
 //SAMPLE API FUNCTIONS
 
 //Search by Product Description
 const fda = (query) => {
   let formatted = query.split(' ').join('+');
-  axios.get(`https://api.fda.gov/food/enforcement.json?api_key=${API_KEY}&search=product_description:"${formatted}"&limit=10`)
+  axios.get(`https://api.fda.gov/food/enforcement.json?api_key=${config.API_KEY}&search=product_description:"${formatted}"&limit=10`)
     .then((res) => {
       console.log(res.data);
     })
@@ -17,7 +17,7 @@ const fda = (query) => {
 
 //Searching by Date Filter
 const fdaDate = () => {
-  axios.get(`https://api.fda.gov/food/enforcement.json?api_key=${API_KEY}&search=report_date:[2016-01-01+TO+2017-12-31]&limit=10`)
+  axios.get(`https://api.fda.gov/food/enforcement.json?api_key=${config.API_KEY}&search=report_date:[2016-01-01+TO+2017-12-31]&limit=10`)
     .then((res) => {
       console.log(res.data);
     })
