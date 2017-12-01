46620fa4e5ed2e2c3cca6ec671693c9f16fb3d7c
dwrz <dwrz@dwrz.net>
2017-12-01 01:10:42 +0000

    Refactor loadSavedLists to use a dropdown

--- client/src/components/loadSavedLists.jsx
+++ client/src/components/loadSavedLists.jsx
@@ -3,11 +3,15 @@
 import React from 'react';
 
 const loadSavedLists = props => (
-  <div>
-    {props.savedLists.map((listEntry, i) => (
-      <p key={i} onClick={props.getSavedListItems.bind(null, listEntry)}>{listEntry}</p>
-    ))}
+  <div className="dropdown">
+    <button className="btn btn-danger dropdown-toggle" type="button" id="savedLists" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    </button>
+    <div className="dropdown-menu" aria-labelledby="savedLists">
+      {props.savedLists.map((listEntry, i) => (
+        <a className="dropdown-item" href="#" key={i} onClick={props.getSavedListItems.bind(null, listEntry)}>{listEntry}</a>
+      ))}
   </div>
+    </div>
 );
 
 export default loadSavedLists;
