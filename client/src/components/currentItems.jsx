74b40af2d2c529a7a4018d0739636735d605d66f
dwrz <dwrz@dwrz.net>
2017-12-01 03:18:04 +0000

    Refactor currentItems to list items

--- client/src/components/currentItems.jsx
+++ client/src/components/currentItems.jsx
@@ -2,10 +2,14 @@
 
 import React from 'react';
 
-const currentItems = props => (
+const currentItems = props => {
+  /*return (
   <div>
     <p><b>Current Items:</b></p>
+    {props.currentItems.map((item, i) => (
+      <span key={i}>{item} </span>
+    ))}
   </div>
-);
+)*/};
 
 export default currentItems;
