e0c40b37c4909fa4b2d90725298564be4b38d63a
dwrz <dwrz@dwrz.net>
2017-12-01 01:15:36 +0000

    Refactor dropdown to display selection text

--- client/src/components/loadSavedLists.jsx
+++ client/src/components/loadSavedLists.jsx
@@ -3,14 +3,17 @@
 import React from 'react';
 
 const loadSavedLists = props => (
-  <div className="dropdown">
-    <button className="btn btn-danger dropdown-toggle" type="button" id="savedLists" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    </button>
-    <div className="dropdown-menu" aria-labelledby="savedLists">
-      {props.savedLists.map((listEntry, i) => (
-        <a className="dropdown-item" href="#" key={i} onClick={props.getSavedListItems.bind(null, listEntry)}>{listEntry}</a>
-      ))}
+  <div>
+    <div className="dropdown">
+      <button className="btn btn-danger dropdown-toggle text-center" type="button" id="savedLists" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Load a saved list
+      </button>
+      <div className="dropdown-menu" aria-labelledby="savedLists">
+        {props.savedLists.map((listEntry, i) => (
+          <a className="dropdown-item" href="#" key={i} onClick={props.getSavedListItems.bind(null, listEntry)}>{listEntry}</a>
+        ))}
   </div>
+    </div>
     </div>
 );
 
