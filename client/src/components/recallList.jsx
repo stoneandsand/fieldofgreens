466615e7497df47c599dfeff45c06afb1424ec1d
dwrz <dwrz@dwrz.net>
2017-12-02 19:35:34 +0000

    Add grid to recallList

--- client/src/components/recallList.jsx
+++ client/src/components/recallList.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 import RecallListItem from './recallListItem.jsx';
 
 const recallList = props => (
-  <div className="card mb-4">
-    <div className="recallList">
-      <h4 className="card-header">
-        <b>Recalls</b>
-      </h4>
+  <div className="row">
+    <div className="col-1">
     </div>
-    <div className="card-body">
-      {props.currentItems.map((item, i) =>
-                              <div key={`recallListItem-${i}`}>
-                                  <RecallListItem deleteItem={props.deleteItem} item={item} i={i} />
-                                    {i < props.currentItems.length - 1 && <hr/>}
-                                </div>
-                             )}
+    <div className="col-10">
+      <div className="card w-100 mb-4">
+        <div className="recallList">
+          <h4 className="card-header">
+            <b>Recalls</b>
+          </h4>
+        </div>
+        <div className="card-body">
+          <ul className="list-group list-group-flush">
+          {props.currentItems.map((item, i) =>
+                                  <li className="list-group-item" key={`recallListItem-${i}`}>
+                                      <RecallListItem deleteItem={props.deleteItem} item={item} i={i} />
+                                               </li>
+                                 )}
+  </ul>
   </div>
+    </div>
+    </div>
+    <div className="col-1">
+    </div>
     </div>
 );
 
