274bd21badaaee677551413755988c88deb5d89b
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-02 11:19:52 -0800

    Correct indentation

--- client/src/components/iteminput.jsx
+++ client/src/components/iteminput.jsx
@@ -5,10 +5,10 @@ const itemInput = props => {
   <div>
     <form className="form-group" onSubmit={props.addNewItemToList}>
       <div className="input-group">
-      <input className="form-control" type="text" placeholder="New Item" value={props.newItemEntry} onChange={props.updateNewItemEntry} />
-      <span className="input-group-btn">
-        <input type="submit" className="btn btn-success" value="Add Item" />
-      </span>
+        <input className="form-control" type="text" placeholder="New Item" value={props.newItemEntry} onChange={props.updateNewItemEntry} />
+        <span className="input-group-btn">
+          <input type="submit" className="btn btn-success" value="Add Item" />
+        </span>
       </div>
     </form>
   </div>
