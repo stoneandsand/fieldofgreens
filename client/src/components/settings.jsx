540b2d0ec586c38d8c7afbf9ced1533bf0bcbff6
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-01 18:56:24 -0800

    Create and export Settings component

---
+++ client/src/components/settings.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Settings extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>test</h1>
+    )
+  }
+}
+
+export default Settings;
