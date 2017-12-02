168d691e44389cbeea26e8eeccd97e39a29716a4
dwrz <dwrz@dwrz.net>
2017-12-02 22:02:54 +0000

    Fix styling in recallList

--- client/src/components/recallList.jsx
+++ client/src/components/recallList.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import RecallListItem from './recallListItem.jsx';
 
 const recallList = props => (
-  <div className="row">
+  <div className="row justify-content-md-center">
     <div className="col-1">
     </div>
     <div className="col-10">
-      <div className="card w-100 mb-4">
+      <div className="card mb-4">
         <div className="recallList">
           <h4 className="card-header">
             <b>Recalls</b>
@@ -14,13 +14,13 @@ const recallList = props => (
         </div>
         <div className="card-body">
           <ul className="list-group list-group-flush">
-          {props.currentItems.map((item, i) =>
-                                  <li className="list-group-item" key={`recallListItem-${i}`}>
-                                      <RecallListItem deleteItem={props.deleteItem} item={item} i={i} />
-                                               </li>
-                                 )}
+            {props.currentItems.map((item, i) =>
+                                    <li className="list-group-item" key={`recallListItem-${i}`}>
+                                        <RecallListItem deleteItem={props.deleteItem} item={item} i={i} />
+                                      </li>
+                                   )}
   </ul>
-  </div>
+    </div>
     </div>
     </div>
     <div className="col-1">
