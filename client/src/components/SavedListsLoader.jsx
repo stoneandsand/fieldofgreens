37d0189b406c07d8cf17f7d4f3b4611f40c6c4be
dwrz <dwrz@dwrz.net>
2017-12-03 00:46:13 +0000

    Refactor list loader color

--- client/src/components/SavedListsLoader.jsx
+++ client/src/components/SavedListsLoader.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 const SavedListsLoader = props => (
   <div>
     <div className="dropdown mb-2">
-      <button className="btn btn-warning btn-block dropdown-toggle text-center" type="button" id="savedLists" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <button className="btn btn-success btn-block dropdown-toggle text-center" type="button" id="savedLists" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         Load a saved list
       </button>
       <div className="dropdown-menu" aria-labelledby="savedLists">
