9efc2bbce288ad7ceec7c53e6bb176a5ffd45d81
dwrz <dwrz@dwrz.net>
2017-12-04 20:18:54 +0000

    Refactor SettingsEntry

--- client/src/components/SettingsEntry.jsx
+++ client/src/components/SettingsEntry.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
 
 const SettingsEntry = props => {
-  let click;
-  if (props.like) {
-    click = props.removeLike.bind(props.like);
-  } else if (props.dislike) {
-    click = props.removeDislike.bind(props.dislike);
-  } else if (props.allergy) {
-    click = props.removeAllergy.bind(props.allergy);
-  }
   return (
     <div>
       <div>
-        <button type="button" className="btn btn-danger" onClick={click}>
+        <button type="button" className="btn btn-danger" onClick={props.removeEntry.bind(props.entry)}>
           <i className="fa fa-remove fa-1x" aria-hidden="true"></i>
         </button>
         <span className="settings-item">{props.settings.name}</span>
