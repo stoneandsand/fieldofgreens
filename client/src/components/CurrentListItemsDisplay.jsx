a4a073d174378096b6d0bd677f81c53863a23471
Jonathan Tang <j.tang17@gmail.com>
2017-12-04 11:56:40 -0800

    Add [x] for removing items from Current Items display

--- client/src/components/CurrentListItemsDisplay.jsx
+++ client/src/components/CurrentListItemsDisplay.jsx
@@ -9,7 +9,10 @@ const CurrentListItemsDisplay = props =>
           <p><b>Current Items: </b>
             {props.currentItems.map((item, i) => {
               return (
-                <span key={`currentItems-${i}`} onClick={props.deleteItem.bind(null, props.i)} >{item.name}{props.currentItems.length <= 1 ? '.' : i < props.currentItems.length - 1 ? ', ' : '.'}</span>
+                <span key={`currentItems-${i}`}  >
+                <span className="text-danger" onClick={props.deleteItem.bind(null, props.i)}>[x]
+                </span>{' '}
+      {item.name}{props.currentItems.length <= 1 ? ' ' : i < props.currentItems.length - 1 ? ', ' : ' '}</span>
               );
             })}
         </p>
