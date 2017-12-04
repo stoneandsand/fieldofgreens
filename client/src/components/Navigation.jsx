994253caad7054f31bbb4b2fbb8dc5361ea07f82
dwrz <dwrz@dwrz.net>
2017-12-04 18:29:46 +0000

    Refactor state dropdown to dissapear from nav on login

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -51,7 +51,7 @@ class Navigation extends React.Component {
 
   render() {
     const isLoggedIn = <span>   <button type="button" className="btn btn-info" onClick={this.props.goToSettingsView}><i className="fa fa-cog fa-1x" aria-hidden="true"></i></button>    <button type="button" className="btn btn-warning" onClick={this.logoutFunc}>Logout</button></span>;
-    const isNotLoggedIn = <button type="button" className="btn btn-primary" onClick={this.props.auth.login}>Login / Sign-Up</button>;
+    const isNotLoggedIn = <span> <button type="button" className="btn btn-primary" onClick={this.props.auth.login}>Login / Sign-Up</button></span>;
     return (
       <div className="container-fluid navigation">
         <div className="row top">
@@ -62,7 +62,7 @@ class Navigation extends React.Component {
             <ItemInput addNewItemToList={this.props.addNewItemToList} newItemEntry={this.props.newItemEntry} updateNewItemEntry={this.props.updateNewItemEntry} />
           </div>
           <div className="col-md-4 signUpOrSettings">
-            <StateDropdown location={this.props.location} selectLocation={this.props.selectLocation} />
+            {!this.props.username && <StateDropdown location={this.props.location} selectLocation={this.props.selectLocation}/>}
  {
   localStorage.getItem('authenticated') ? isLoggedIn : isNotLoggedIn
  }
