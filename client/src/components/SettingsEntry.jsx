fcb0d54062c4d8de4cba76f8f39d66ccc783cfde
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-04 12:45:50 -0800

    Give wrapper div className of settingsEntry

--- client/src/components/SettingsEntry.jsx
+++ client/src/components/SettingsEntry.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SettingsEntry = props => {
   return (
-    <div>
+    <div className="settingsEntry">
       <div>
         <button type="button" className="btn btn-danger" onClick={props.removeEntry.bind(null, props.entry)}>
           <i className="fa fa-remove fa-1x" aria-hidden="true"></i>
