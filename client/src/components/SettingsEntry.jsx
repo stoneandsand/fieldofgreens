67700cabbe0a71021956b5358c922bf1c8f4ca23
Charles Paul Bennett <cpbennett4@gmail.com>
2017-12-04 12:40:23 -0800

    Bind onClick to null, correct this.settings.name

--- client/src/components/SettingsEntry.jsx
+++ client/src/components/SettingsEntry.jsx
@@ -4,10 +4,10 @@ const SettingsEntry = props => {
   return (
     <div>
       <div>
-        <button type="button" className="btn btn-danger" onClick={props.removeEntry.bind(props.entry)}>
+        <button type="button" className="btn btn-danger" onClick={props.removeEntry.bind(null, props.entry)}>
           <i className="fa fa-remove fa-1x" aria-hidden="true"></i>
         </button>
-        <span className="settings-item">{props.settings.name}</span>
+        <span className="settings-item">{props.entry}</span>
       </div>
     </div>
   );
