89d0a26934f76a24f6656995e1e550d42989c7d7
Jonathan Tang <j.tang17@gmail.com>
2017-12-04 16:43:18 -0800

    Run eslint and remove consoles, refactor styling in db/server

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
 const recalls = require('../db/data.js');
@@ -6,7 +7,7 @@ const db = require('../db/index.js');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(`${__dirname}/../`));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // HELPERS
@@ -14,22 +15,21 @@ app.use(bodyParser.json());
 // Helper function for getting data that matches the recalls GET request`
 const getRecallMatches = (keywordsArray) => {
   let matches = [];
-  for (let keyword of keywordsArray) {
-    for (let recall of recalls.recallData ) {
-      if (recall.product_description.toUpperCase().includes(keyword.toUpperCase()) && recall.report_date > '20160101' && recall.status ==='Ongoing') {
+  keywordsArray.forEach((keyword) => {
+    recalls.recallData.forEach((recall) => {
+      if (recall.product_description.toUpperCase().includes(keyword.toUpperCase()) && recall.report_date > '20160101' && recall.status === 'Ongoing') {
         matches.push(recall);
       }
-    }
-  }
+    });
+  });
+
   matches = matches.filter(match => match.status === 'Ongoing');
   return matches;
 };
 
 // ROUTING
 app.post('/signup', (req, res) => {
-  console.log(req.body, 'req.body for signup');
   // expecting {username: '', password: ''}
-
   db.signup(req.body.email, (err, username) => {
     if (err) {
       res.send('');
@@ -41,34 +41,28 @@ app.post('/signup', (req, res) => {
 // GET request for getting recall data from data.js
 app.get('/api/search/:location/:item', (req, res) => {
   // expecting {item: 'cheese cake', location: 'CA'}
-
   const keywords = req.params.item.split(' ');
-  console.log(keywords);
-
   let matches = getRecallMatches(keywords);
 
   matches = matches.filter(match => match.distribution_pattern.includes(req.params.location));
   res.send(matches.slice(0, 10));
 });
-// || match.distribution_pattern.includes('Nationwide')
 
 // POST request for saving new list to database
 app.post('/api/users/:username/lists', (req, res) => {
   const items = [];
-  for (const item of req.body.items) {
+  req.body.items.forEach((item) => {
     items.push(item.name);
-  }
+  });
   const list = {
     name: req.body.listName,
-    items: items,
+    items,
   };
-  console.log(req.params.username)
-  console.log(list);
+
   db.saveList(req.params.username, list, (err, updatedLists) => {
     if (err) {
-      res.send(err); // Error retrieving updated lists
+      res.send(err); // Error retrieving updated lists NEVER SEND ERR
     }
-    console.log(updatedLists);
     res.send(updatedLists);
   });
 });
@@ -78,8 +72,9 @@ app.get('/api/users/:username/lists', (req, res) => {
   db.getUserLists(req.params.username, (err, userLists) => {
     if (err) {
       res.send([]); // Error retrieving saved lists
+    } else {
+      res.send(userLists);
     }
-    res.send(userLists);
   });
 });
 
@@ -88,9 +83,8 @@ app.get('/api/users/:username/lists/:id', (req, res) => {
   // each saved list should have a _id property
   db.findList(req.params.username, req.params.id, (err, targetList) => {
     if (err) {
-      res.send({}); //Error retrieving list
+      res.send({}); // Error retrieving list
     }
-    console.log(targetList);
     res.send(targetList);
   });
 });
@@ -98,20 +92,20 @@ app.get('/api/users/:username/lists/:id', (req, res) => {
 app.get('/api/users/:username/settings', (req, res) => {
   db.findSettings(req.params.username, (err, settings) => {
     if (err) {
-      res.send({allergies: [], likes: [], dislikes: [], location: ''});
-      //Error retrieving user settings
+      res.send({
+        allergies: [], likes: [], dislikes: [], location: '',
+      });
     }
-  //should send {allergies: [], likes: [], dislikes: [], location: ''}
+    // should send {allergies: [], likes: [], dislikes: [], location: ''}
     res.send(settings);
-  })
+  });
 });
 
 app.post('/api/users/:username/allergies', (req, res) => {
-  console.log(req.body, 'req.body');
   // Expecting {item: 'apple'} item in POST request
   db.addAllergies(req.body, (err, updatedAllergyList) => {
     if (err) {
-      res.send([]); //Error retrieving updated allergy list
+      res.send([]); // Error retrieving updated allergy list
     }
     res.send(updatedAllergyList);
   });
@@ -121,7 +115,7 @@ app.post('/api/users/:username/likes', (req, res) => {
   // Expecting {item: 'apple'} item in POST request
   db.addLikes(req.body, (err, updatedLikeList) => {
     if (err) {
-      res.send([]); //Error retrieving updated likes list
+      res.send([]); // Error retrieving updated likes list
     }
     res.send(updatedLikeList);
   });
@@ -131,7 +125,7 @@ app.post('/api/users/:username/dislikes', (req, res) => {
   // Expecting {item: 'apple'} item in POST request
   db.addDislikes(req.body, (err, updatedDislikeList) => {
     if (err) {
-      res.send([]); //Error retrieving updated dislikes list
+      res.send([]); // Error retrieving updated dislikes list
     }
     res.send(updatedDislikeList);
   });
@@ -140,9 +134,9 @@ app.post('/api/users/:username/dislikes', (req, res) => {
 app.post('/api/users/:username/location', (req, res) => {
   db.addLocation(req.params.username, req.body.location, (err, location) => {
     if (err) {
-      res.send(''); //Error saving location
+      res.send(''); // Error saving location
     }
-    res.send(location)
+    res.send(location);
   });
 });
 
@@ -150,7 +144,7 @@ app.post('/api/users/:username/delete', (req, res) => {
   // Expecting {name: 'apple', type: 'allergies'}
   db.removeItem(req.params.username, req.body, (err, updatedList) => {
     if (err) {
-      res.send([]); //Error retrieving updated list
+      res.send([]); // Error retrieving updated list
     }
     res.send(updatedList);
   });
